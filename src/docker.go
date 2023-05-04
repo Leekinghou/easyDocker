4602bb280849503903c6e3a9802c06837c614f6a
lijinhao <lijinhao716@163.com>
2023-05-04 21:49:11 +0800

    Use make && makefile

--- src/docker.go
+++ src/docker.go
@@ -1,25 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"syscall"
 )
 
 func main() {
+	fmt.Printf("Process => %v [%d]\n", os.Args, os.Getpid())
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
-		panic("have not define")
+		panic("have not defined")
 	}
 }
 
 func run() {
-	//fmt.Println(os.Args[2])
-	cmd := exec.Command(os.Args[2])
+	cmd := exec.Command(os.Args[2], append([]string{"child"}, os.Args[2])...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS, // linux特有
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -27,4 +30,13 @@ func run() {
 	if err := cmd.Run(); err != nil {
 		panic(err)
 	}
+}
+
+func child() {
+	cmd := exec.Command(os.Args[2])
+	syscall.Sethostname([]byte("container"))
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
